8ac1ac7cfbaa60ae025a643b5b3ce4b4802eddf8
jchg12 <68677407+jchg12@users.noreply.github.com>
2020-08-13 03:33:06 -0400

    configuration

--- ControlTienda/ControlTienda/ControlTienda/Migrations/Configuration.cs
+++ ControlTienda/ControlTienda/ControlTienda/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 ﻿namespace ControlTienda.Migrations
 {
+    using ControlTienda.Data.Entities;
+    using ControlTienda.Encrypt;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,6 +16,25 @@
 
         protected override void Seed(ControlTienda.Data.DataContext context)
         {
+         
+            Rol rol = new Rol();
+            User user = new User();
+            Encrypting encrypting = new Encrypting();
+
+            if (context.Rols == null && context.Users == null)
+            {
+                rol.Name = "Manager";
+                rol.Details = "This Rol has acces to all the System.";
+                context.Rols.Add(rol);
+
+                user.Nickname = "Manager";
+                user.Name = "Manager";
+                user.Password = encrypting.GetSHA256("Manager");
+                user.Rol = rol;
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
