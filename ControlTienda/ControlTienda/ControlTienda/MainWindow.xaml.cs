0a080e4e6cd08358d273b591f0345e4632e0385f
jchg12 <68677407+jchg12@users.noreply.github.com>
2020-08-13 03:16:36 -0400

    cambios

--- ControlTienda/ControlTienda/ControlTienda/MainWindow.xaml.cs
+++ ControlTienda/ControlTienda/ControlTienda/MainWindow.xaml.cs
@@ -1,21 +1,22 @@
-﻿using ControlTienda.Encrypt;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ControlTienda
+﻿namespace ControlTienda
 {
+    using ControlTienda.Data;
+    using ControlTienda.Data.Entities;
+    using ControlTienda.Encrypt;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
@@ -28,10 +29,38 @@ namespace ControlTienda
 
         private void btnAceptar_Click(object sender, RoutedEventArgs e)
         {
-            string pass;
-            pass = tbPassword.Text;
+            DataContext context = new DataContext();
+            GenericRepository<User> generic = new GenericRepository<User>(context);
             Encrypting en = new Encrypting();
-            MessageBox.Show(en.GetSHA256(pass));
+            string nick = TbUserName.Text;
+            var user = (from u in context.Users
+                        where u.Nickname == nick
+                        select u).FirstOrDefault();
+            if (!(user == null))
+            {
+                if (!generic.Exist(user.Id))
+                    MessageBox.Show("User does not exist...", "WARNING!!!");
+                else
+                {
+                    string pass = TbPassword.Password;
+                    if (en.GetSHA256(pass) == user.Password)
+                    {
+                        MessageBox.Show("Logueado... " + user.Name);
+                    }
+                    else
+                        MessageBox.Show("Incorrect Password");
+                }
+            }
+            else
+            {
+                MessageBox.Show("User does not exist...", "WARNING!!!");
+            }
+
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
